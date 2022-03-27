232e121e1052c43d26bbbc36b1981fd1e27e14bd
Ammar Mahmoud <ammar.m777.2@gmail.com>
2022-03-28 01:12:03 +0200

    initial commit

---
+++ Controllers/UserAuth.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTechTree.Controllers
+{
+    public class UserAuth : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        public UserAuthController(SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
